22c4b1d3033d8d2ad1f6a333baa21a2c7c08374f
Milosz03 <m.malota1234@gmail.com>
2022-01-06 16:06:57 +0100

    commit ser 02

--- server01.js
+++ server01.js
@@ -3,8 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
-  res.send("<h1>my app on heroku! mała zmiana w pliku</h1>");
-
+  res.send("<h1>Miłosz Małota 3i2</h1>");
 });
 
 app.listen(PORT, function () {
